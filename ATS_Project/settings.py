7530157e6c3a1bf2a61fcf3fbba8b5a08e397a70
fmoges@excellerentsolutions.com <fmoges@excellerentsolutions.com>
2022-06-23 18:33:33 +0300

    mutable change

--- ATS_Project/settings.py
+++ ATS_Project/settings.py
@@ -134,32 +134,32 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ATS_Project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'atsdb',
-#         'USER': 'root',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': 3306,
-#
-#     }
-# }
-
+#Database
+#https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'de0ec6nf2cs7gb',
-        'USER': 'eibfkgkzyhcqvd',
-        'PASSWORD': 'eb9a37dd4b5a9303300b578373bcb5eae8bde3215179c7c4d558873887b19a56',
-        'HOST': 'ec2-34-225-159-178.compute-1.amazonaws.com',
-        'PORT': '5432',
-          }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'atsdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 3306,
+
+    }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'de0ec6nf2cs7gb',
+#         'USER': 'eibfkgkzyhcqvd',
+#         'PASSWORD': 'eb9a37dd4b5a9303300b578373bcb5eae8bde3215179c7c4d558873887b19a56',
+#         'HOST': 'ec2-34-225-159-178.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#           }
+# }
 
 
 # Password validation
